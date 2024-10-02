3f92c9a610cccf57501967b4c67a715990566f64
cathydomini <cathydomini@yahoo.com>
2024-10-02 13:43:29 -0700

    azure function

--- AzureFunction/AzureFunction.cs
+++ AzureFunction/AzureFunction.cs
@@ -10,9 +10,9 @@ using Newtonsoft.Json;
 
 namespace FunctionApp1
 {
-    public static class Function1
+    public static class AzureFunction
     {
-        [FunctionName("Function1")]
+        [FunctionName("AzureFunction")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
